571f59a90b5e389b24328a7086d5d7d7312c0543
AliAhmed1973 <eng_aliahmed1973@yahoo.com>
2021-09-20 20:05:55 +0200

    Create account settings screen

---
+++ app/src/main/java/com/aliahmed1973/preferencekotlin/AccountSettingsFragment.kt
@@ -0,0 +1,17 @@
+package com.aliahmed1973.preferencekotlin
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.preference.PreferenceFragmentCompat
+
+
+class AccountSettingsFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.account_settings, rootKey)
+    }
+
+
+}
